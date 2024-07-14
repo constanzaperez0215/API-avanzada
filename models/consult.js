b854b7b273ca3b9a7d0c0c806d8a4ac57d19b426
Constanza Pérez <constanza.vp.o@gmail.com>
2024-07-14 17:37:54 -0400

    Se agrega la paginación

--- models/consult.js
+++ models/consult.js
@@ -1,9 +1,10 @@
 import { db } from '../db/configDB.js'
 import format from 'pg-format'
 
-export const findAll = async ({ limits = 3, orderBy = 'precio_DESC' }) => {
+export const findAll = async ({ limits = 3, orderBy = 'precio_DESC', page = 1 }) => {
   const [column, sort] = orderBy.split('_')
-  const formattedQuery = format('SELECT * FROM inventario ORDER BY %s %s LIMIT %s;', column, sort.toUpperCase(), limits)
+  const offset = Math.abs(page > 0 ? page - 1 : 0) * limits
+  const formattedQuery = format('SELECT * FROM inventario ORDER BY %s %s LIMIT %s OFFSET %s;', column, sort.toUpperCase(), limits, offset)
   return await db(formattedQuery)
 }
 
